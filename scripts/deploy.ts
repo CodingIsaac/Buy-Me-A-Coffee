8eb200e0e3524da4d6ff2c3377f633101498d4f8
CodingIsaac <isaacijuo@gmail.com>
2022-11-01 14:21:29 +0000

    Interaction Commit

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,24 @@
 import { ethers } from "hardhat";
+const hre = require("hardhat");
+
+async function getBalance(address: any) {
+  const balance = await hre.ethers .provider.getBalance(address);
+  return hre.ethers.formatEther(balance)
+  
+}
+async function printBalance(addresses: any) {
+  let index = 0;
+  for (const address of addresses) {
+    console.log(`Address ${index} balance:`, await getBalance(address));
+    index++;
+
+  }
+
+  
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
